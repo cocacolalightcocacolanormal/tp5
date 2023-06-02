ba7d794d84b77d9fcddfecd069e72fe9cb41ab3a
Karl Samaha <samahak@educ.college-montreal.qc.ca>
2023-06-02 10:49:05 -0400

    Initial commit

---
+++ gamestate.py
@@ -0,0 +1,7 @@
+if self.game_state == GameState.RenderInventory:
+	# Dessiner la fenêtre de l'inventaire
+	pass
+elif self.game_state == GameState.RenderMenu:
+	# Dessiner le menu principal
+	pass
+
